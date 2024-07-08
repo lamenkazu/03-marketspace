1c4f98374e13946668635386b990f97a4300c547
Lamenkazu <333.erick@gmail.com>
2024-07-08 09:18:24 -0300

    ✏️🖼️ Melhoria visual Search Advertising

--- src/screens/app/Home/components/SearchAdvert.tsx
+++ src/screens/app/Home/components/SearchAdvert.tsx
@@ -12,12 +12,13 @@ import { ComponentProps } from 'react'
 
 interface SearchAdvertProps extends ComponentProps<typeof Input> {}
 
+/** Algumas propriedaes apresentam erro de tipagem, mas funcionam corretamente na UI. Problema com Gluestack */
+
 export const SearchAdvert = ({ ...props }: SearchAdvertProps) => {
   return (
     <Input
       w={'$full'}
       h={45}
-      pr={16}
       mb={24}
       borderRadius={6}
       bg={'$gray700'}
@@ -25,19 +26,30 @@ export const SearchAdvert = ({ ...props }: SearchAdvertProps) => {
       alignItems="center"
       {...props}
     >
-      <InputField flex={1} />
+      <InputField selectionColor={'#647AC766'} flex={1} />
 
-      <InputSlot onPress={() => console.log('search')} $active-opacity={0.8}>
-        <InputIcon>
-          <Icon as={MagnifyingGlass} weight={'bold'} />
+      <InputSlot
+        py={10}
+        px={5}
+        mr={0}
+        onPress={() => console.log('search')}
+        $active-opacity={0.8}
+      >
+        <InputIcon size={'lg'}>
+          <Icon as={MagnifyingGlass} weight={'bold'} size={'lg'} />
         </InputIcon>
       </InputSlot>
 
       <Divider orientation="vertical" mx="$2.5" bg="$gray400" h={16} />
 
-      <InputSlot onPress={() => console.log('filter')} $active-opacity={0.8}>
-        <InputIcon>
-          <Icon weight={'bold'} as={Sliders} />
+      <InputSlot
+        py={10}
+        px={5}
+        onPress={() => console.log('filter')}
+        $active-opacity={0.8}
+      >
+        <InputIcon size="lg">
+          <Icon as={Sliders} weight={'bold'} size={'lg'} />
         </InputIcon>
       </InputSlot>
     </Input>
